0bfcd3e6d8870b5b41c1d50c0d65baf13c2606d9
tahirshaikh099 <57958180+tahirshaikh099@users.noreply.github.com>
2024-02-29 12:23:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path')
 const fs = require('fs').promises;
 
 const app = express();
@@ -9,16 +10,26 @@ const PORT = 3000;
 app.use(bodyParser.json());
 
 
-app.post('/order/:number', async (req, res) => {
+app.post('/order', async (req, res) => {
     let number = req.params.number;
     try {
-        await fs.appendFile(`${number}.json`, JSON.stringify(req.body, null, 2));
+        await fs.appendFile('data.json', JSON.stringify(req.body, null, 2));
         res.sendStatus(200);
     } catch (error) {
         console.error('Error saving data:', error);
         res.sendStatus(500);
     }
 });
+
+app.get('/data', async (req, res) => {
+    try {
+        // await fs.readFile('data.json', JSON.stringify(req.body, null, 2));
+        res.sendFile(path.join(__dirname, '/', 'data.json'));
+    } catch (error) {
+        console.error('Error saving data:', error);
+        res.sendStatus(500);
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
